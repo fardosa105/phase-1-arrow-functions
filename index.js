a178585f31961baf78152c1d66815745cc054efd
+ <fadosa.abdirhman@student.moringaschool>
2024-07-05 13:11:44 +0300

    modify

---
+++ index.js
@@ -0,0 +1,22 @@
+// Define the function expression called divide
+const divide = function() {
+    return 2000 / 100;
+  };
+  
+  // Define the arrow function called square
+  const square = (num) => {
+    return num * num;
+  };
+  
+  // Define the arrow function called add
+  const add = (a, b) => {
+    return a + b;
+  };
+  
+  // Export the functions
+  module.exports = {
+    divide,
+    square,
+    add
+  };
+  
